06136f4ec7369ed883572888f63a3cfe9f74c89c
Sterling Hanenkamp <hanenkamp@cpan.org>
2008-12-14 22:53:40 -0600

    releng: bumping to 0.04 and noting changes

--- lib/Net/Google/PicasaWeb.pm
+++ lib/Net/Google/PicasaWeb.pm
@@ -21,11 +21,11 @@ Net::Google::PicasaWeb - use Google's Picasa Web API
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
