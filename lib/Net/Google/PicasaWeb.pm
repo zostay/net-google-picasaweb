b7380ddde36af42a6ad2790f0021314ea13717d7
Sterling Hanenkamp <hanenkamp@cpan.org>
2008-12-21 18:03:04 -0600

    releng: updated Changes and version bump to 0.05

--- lib/Net/Google/PicasaWeb.pm
+++ lib/Net/Google/PicasaWeb.pm
@@ -21,11 +21,11 @@ Net::Google::PicasaWeb - use Google's Picasa Web API
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 =head1 SYNOPSIS
 
